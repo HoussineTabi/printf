f3b4a719fa6b65a6ebcafdcb3747eddf52dea4a6
HoussineTabi <0620036304ht@gmail.com>
2023-04-24 15:46:55 -0700

    _printfunc

---
+++ _printf.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * _printf - is a funciton that print differnt type of data
+ * @format: the first parameter to specifie the type of data
+ * Return: an integer to success
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int nu;
+	char *p = format;
+	char c;
+	int i;
+	char *pr;
+	va_start(ap, format);
+	while (*p != '\0')
+	{
+		if (*p == '%')
+		{
+			p++;
+			switch (*p)
+			{
+			case 's':
+				pr = va_arg(ap, char*);
+				while (*pr != '\0')
+				{
+					i += wrchar(*pr);
+					pr++;
+				}
+				break;
+			case 'i':
+				nu = va_arg(ap, int);
+				i += 4;
+				_print_number(nu);
+				break;
+			case 'd':
+				nu = va_arg(ap, int);
+				i += 4;
+				_print_number(nu);
+				break;
+			case 'b':
+				nu = va_arg(ap, int);
+				_binar(nu);
+				break;
+			case 'u':
+				nu = va_arg(ap, int);
+				_unsig(nu);
+				break;
+			case 'o':
+				nu = va_arg(ap, int);
+				_octal(nu);
+				break;
+			case 'X':
+				nu = va_arg(ap, int);
+				_hexupper(nu);
+				break;
+			case 'x':
+				nu = va_arg(ap, int);
+				_hexlower(nu);
+				break;
+			default:
+				i += wrchar(*(p - 1));
+				i += wrchar(*p);
+				break;
+			}
+		}
+		else
+			i += wrchar(*p);
+		p++;
+	}
+	va_end(ap);
+	return (i);
+}

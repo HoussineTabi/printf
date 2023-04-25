bed9f3fd6ecb3ec593a02712f0186d4bfc08453b
HoussineTabi <0620036304ht@gmail.com>
2023-04-25 02:24:01 -0700

    addheader

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <limits.h>
+int _printf(const char *foramt, ...);
+void _print_number(int);
+int wrchar(int);
+int _print_string(char*);
+void _binar(int);
+void _octal(unsigned int);
+void _hexupper(unsigned int);
+void _hexlower(unsigned int);
+void _unsig(int);
+void _print_numberu(unsigned int);
+
+#endif /* MAIN_H */
